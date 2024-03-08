5df57e17f411d9283bba1c65ea47ba90ac84108e
Francis Tsao <dialeleven@gmail.com>
2024-03-08 10:08:01 -0500

    test

--- oop_08_namespaces.php
+++ oop_08_namespaces.php
@@ -8,4 +8,5 @@ They allow for better organization by grouping classes that work together to per
 They allow the same name to be used for more than one class
 For example, you may have a set of classes which describe an HTML table, such as Table, Row and Cell while also having another set of classes to describe furniture, such as Table, Chair and Bed. Namespaces can be used to organize the classes into two different groups while also preventing the two classes Table and Table from being mixed up.
 */
+
 ?>
