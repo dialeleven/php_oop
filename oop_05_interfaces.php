8c00063653dfc78217e3c131886a1243f5ac4814
Francis Tsao <dialeleven@gmail.com>
2024-03-07 21:27:28 -0500

    more on interfaces

--- oop_05_interfaces.php
+++ oop_05_interfaces.php
@@ -42,16 +42,59 @@ class Cat implements Animal {
      echo "Meow\n";
    }
 }
-
-class Dog implements Animal {
-   public function makeSound() {
-      echo "Woof woof!\n";
-   }
-}
  
- $animal = new Cat();
- $animal->makeSound();
+$animal = new Cat();
+$animal->makeSound();
 
- $animal2 = new Dog();
- $animal2->makeSound();
+
+
+/*
+From the example above, let's say that we would like to write software which 
+manages a group of animals. There are actions that all of the animals can do, but 
+each animal does it in its own way.
+
+Using interfaces, we can write some code which can work for all of the animals 
+even if each animal behaves differently:
+*/
+// Interface definition
+interface Animal2 {
+  public function makeSound();
+}
+
+// Class definitions
+class Cat2 implements Animal2 {
+  public function makeSound() {
+    echo " Meow ";
+  }
+}
+
+class Dog2 implements Animal2 {
+  public function makeSound() {
+    echo " Bark ";
+  }
+}
+
+class Mouse2 implements Animal2 {
+  public function makeSound() {
+    echo " Squeak ";
+  }
+}
+
+// Create a list of animals
+$cat = new Cat2();
+$dog = new Dog2();
+$mouse = new Mouse2();
+$animals = array($cat, $dog, $mouse);
+
+// Tell the animals to make a sound
+foreach($animals as $animal) {
+  $animal->makeSound();
+}
+/*
+Example Explained
+
+Cat, Dog and Mouse are all classes that implement the Animal interface, which means that all of them are able to make a sound using the makeSound() method. Because of this, we can loop through all of the animals and tell them to make a sound even if we don't know what type of animal each one is.
+
+Since the interface does not tell the classes how to implement the method, each animal can make a sound in its own way.
+*/
 ?>
