82736ae7aeed66c9170c49956c0402129327aa9e
Francis Tsao <dialeleven@gmail.com>
2024-03-08 12:49:20 -0500

    namespace

--- oop_08_namespaces.php
+++ oop_08_namespaces.php
@@ -4,9 +4,19 @@ PHP Namespaces
 
 Namespaces are qualifiers that solve two different problems:
 
-They allow for better organization by grouping classes that work together to perform a task
-They allow the same name to be used for more than one class
-For example, you may have a set of classes which describe an HTML table, such as Table, Row and Cell while also having another set of classes to describe furniture, such as Table, Chair and Bed. Namespaces can be used to organize the classes into two different groups while also preventing the two classes Table and Table from being mixed up.
-*/
+- They allow for better organization by grouping classes that work together to perform a task
+- They allow the same name to be used for more than one class
 
+For example, you may have a set of classes which describe an HTML table, such as 
+Table, Row and Cell while also having another set of classes to describe furniture, 
+such as Table, Chair and Bed. Namespaces can be used to organize the classes into 
+two different groups while also preventing the two classes Table and Table from being mixed up.
+
+Namespaces are declared at the beginning of a file using the 'namespace' keyword.
+*/
+namespace Html;
+
+class Table {
+   
+}
 ?>
